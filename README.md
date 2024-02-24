7d5c2da0bf7dbf8acde2bc94df26f8c5a73ba9be
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2024-02-25 02:13:20 +0530

    Add basic P0 (first 3 lectures of Sentdex)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ai
+# AI
+
+## Neural Networks
+[Active Playlist](https://www.youtube.com/watch?v=Wo5dMEP_BbI&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3)
+
+- Interconnected nodes structured in layers. Each node is a neuron and is connected to all nodes in back & front layers.
+- Aim is simple to take input and produce output. So 2 main layers are input and output. The layers in between are called hidden layers.
+- Tensors are objects which can be represented as arrays.
---
+++ p0.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+# all this for 1 neuron
+inputs = [1, 2, 3, 2.5]  # outputs from previous neuron layer of 3 nodes
+weights = [0.2, 0.8, -0.5, 1]  # weights for each input
+bias = 2.0  # bias for the neuron
+
+output = (
+    inputs[0] * weights[0]
+    + inputs[1] * weights[1]
+    + inputs[2] * weights[2]
+    + inputs[3] * weights[3]
+    + bias
+)
+
+print(output)
+
+# coding for 3 neurons of a layer, these value are we tune
+weights1 = [0.2, 0.8, -0.5, 1]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2.0
+bias2 = 3.0
+bias3 = 0.5
+
+outputMultiNeurons = [
+    inputs[0] * weights1[0]
+    + inputs[1] * weights1[1]
+    + inputs[2] * weights1[2]
+    + inputs[3] * weights1[3]
+    + bias1,
+    inputs[0] * weights2[0]
+    + inputs[1] * weights2[1]
+    + inputs[2] * weights2[2]
+    + inputs[3] * weights2[3]
+    + bias2,
+    inputs[0] * weights3[0]
+    + inputs[1] * weights3[1]
+    + inputs[2] * weights3[2]
+    + inputs[3] * weights3[3]
+    + bias3,
+]
+
+print(outputMultiNeurons)
+
+weightsAll = [weights1, weights2, weights3]
+biasAll = [bias1, bias2, bias3]
+
+# multiple each element of the input with each element of the weights and sum them up
+npOutputSingleNeuron = np.dot(inputs, weights) + bias
+print(npOutputSingleNeuron)
+
+npOutputMultiNeurons = np.dot(weightsAll, inputs) + biasAll
+# 3 by 4 matrix dot 4 by 1 matrix = 3 by 1 matrix
+# np.dot(weightsAll, inputs) = [np.dot(weights1, inputs), np.dot(weights2, inputs), np.dot(weights3, inputs)]
+print(npOutputMultiNeurons, np.shape(weightsAll), np.shape(np.array(weightsAll).T))
